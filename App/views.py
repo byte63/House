a4e843ef1b9c6f660718153ea333e693c3a70583
jonathan-hxj <jonathan.hxj@gmail.com>
2021-10-28 14:58:35 +0800

    add 404 and icon

--- App/views.py
+++ App/views.py
@@ -6,6 +6,11 @@ from App.models import HouseListing
 house = Blueprint('house', __name__)
 
 
+@house.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 @house.route("/")
 def index():
     return redirect(url_for("house.listing", page=1))
