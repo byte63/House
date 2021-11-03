1afdacb53f1ea2e3e43700fb2972bca5e16d5ce1
jonathan-hxj <jonathan.hxj@gmail.com>
2021-11-03 14:29:37 +0800

    添加访问频率限制

--- App/ext.py
+++ App/ext.py
@@ -1,10 +1,17 @@
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 db = SQLAlchemy()
 migrate = Migrate()
+limiter = Limiter(
+    key_func=get_remote_address,
+    default_limits=["5 per minute"]
+)
 
 
 def init_ext(app):
     db.init_app(app)
     migrate.init_app(app, db)
+    limiter.init_app(app)
